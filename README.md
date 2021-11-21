8ee79d1939b6948e1ccd69579919b53c3171b59b
unknown <athithanambia@cloudix.in>
2021-11-21 22:07:13 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # aws-terraform-tutorial
+
+PPT url: https://docs.google.com/presentation/d/1ad_XBHnCmDu7lN6LhMp4gXNV5_NaE7teXDNbDVk7_Bo/edit?usp=sharing
--- first_ec2.tf
+++ first_ec2.tf
@@ -9,13 +9,19 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAWYR4FHO3BQ5VTAQB"
-  secret_key = "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"
+  access_key = "AKIAWYR4FHO3HWWKJHOM"
+  secret_key = "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"
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-0567e0d2b4b2169ae"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "web1" {
+  ami           = "ami-0567e0d2b4b2169ae"
+  instance_type = "t2.micro"
+}
+
 
 
